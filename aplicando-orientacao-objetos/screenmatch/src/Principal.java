98028ce5f34a45db8dba868e8d29bcadf450e7e2
9001518 <davidlimalourenco@gmail.com>
2023-07-11 17:15:11 -0300

    Aula 1

--- aplicando-orientacao-objetos/screenmatch/src/Principal.java
+++ aplicando-orientacao-objetos/screenmatch/src/Principal.java
@@ -5,6 +5,7 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
 import java.io.SequenceInputStream;
+import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
@@ -54,5 +55,23 @@ public class Principal {
         episodio.setSerie(lost);
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
+
+        Filme filmeDoPaulo = new Filme();
+        filmeDoPaulo.setNome("Dogville");
+        filmeDoPaulo.setAnoDeLancamento(2003);
+        filmeDoPaulo.setDuracaoEmMinutos(180);
+        filmeDoPaulo.setIncluidoNoPlano(true);
+
+
+        System.out.println("--------- Mexendo com Arrays ---------");
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+        listaDeFilmes.add(filmeDoPaulo);
+        System.out.println("Tamanho da lista: " + listaDeFilmes.size());
+        System.out.println("Primeiro Filme: " + listaDeFilmes.get(0).getNome());
+
+        System.out.println(listaDeFilmes); //tentando imprimir todos os elementos
+        System.out.println("toString do Filme: " + filmeDoPaulo);
         }
 }
--- aplicando-orientacao-objetos/screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
+++ aplicando-orientacao-objetos/screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -16,4 +16,10 @@ public class Filme extends Titulo implements Classificavel {
     public int getClassificacao() {
         return (int)pegaMedia() / 2;
     }
+
+    @Override
+    public String toString() { //reescrevendo o método da superclasse Object
+        return "Filme: " + this.getNome() + " ("+ this.getAnoDeLancamento() + ")" ;
+        //return super.toString(); isso devolveria o que a superclasse (mãe) devolve
+    }
 }
